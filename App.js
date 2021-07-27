d6fc5f5be2edf0f531b34ed8d55e3e0ea17da416
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-07-27 10:24:02 +0700

    Render list with map function

--- App.js
+++ App.js
@@ -3,30 +3,26 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   // Properties
-  const [counter, setCounter] = useState(0);
-  const [name, setName] = useState("This is my Name");
+  const [items, setItems] = useState([
+    { key: 1, title: "title 1" },
+    { key: 2, title: "title 2" },
+    { key: 3, title: "title 3" },
+    { key: 4, title: "title 4" },
+    { key: 5, title: "title 5" },
+    { key: 6, title: "title 6" },
+    { key: 7, title: "title 7" },
+    { key: 8, title: "title 8" },
+  ]);
 
-  // Function
-  const decreaseCounter = () => {
-    setCounter(counter - 1);
-  };
-
-  const increaseCounter = () => {
-    setCounter(counter + 1);
-  };
-
-  // main() function
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={styles.view1}>View 1</Text>
-      </View>
-      <View>
-        <Text style={styles.view2}>View 2</Text>
-      </View>
-      <View>
-        <Text style={styles.view3}>View 3</Text>
-      </View>
+      {items.map((item) => {
+        return (
+          <View style={styles.item} key={item.key}>
+            <Text style={styles.text}>Title: {item.title}</Text>
+          </View>
+        );
+      })}
     </View>
   );
 }
@@ -38,37 +34,20 @@ const styles = StyleSheet.create({
     flex: 1,
     // flexDirection: "column-reverse",
     // flexDirection: "row",
-    flexDirection: "row-reverse",
+    // flexDirection: "row-reverse",
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
-  view1: {
-    // flex: 1,
-    height: 100,
-    width: 100,
-    backgroundColor: "#00ffff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  view2: {
-    // flex: 1,
-    height: 100,
-    width: 100,
-    backgroundColor: "#0000ff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  view3: {
-    // flex: 1,
-    height: 100,
-    width: 100,
-    backgroundColor: "#00ff00",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
+  item: {
+    margin: 10,
     backgroundColor: "red",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  text: {
     fontSize: 20,
+    textTransform: "uppercase",
   },
 });
