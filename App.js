b9f6e39bfc4bf9a72540eac4e70b62258b8ccb71
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-07-27 09:43:41 +0700

    Init files

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  // Properties
+  const [counter, setCounter] = useState(0);
+  const [name, setName] = useState("This is my Name");
+
+  // Function
+  const decreaseCounter = () => {
+    setCounter(counter - 1);
+  };
+
+  const increaseCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  // main() function
   return (
+    // View = <div class='container'>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello Wolrd !!!</Text>
+      <Text
+      // style={{
+      //   backgroundColor: "blue",
+      // }}
+      >
+        Counter: {counter}
+      </Text>
+      <Text>{name}</Text>
+      <Button onPress={increaseCounter} title="Increase" color="#006400" />
+      <Button onPress={decreaseCounter} title="Decrease" />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +39,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    backgroundColor: "red",
+    fontSize: 20,
   },
 });
