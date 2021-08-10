13ba04a1eb4a9e11000a16c60ffb0993af2cff1c
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-08-10 11:17:31 +0700

    Options for DrawerNavigation

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
@@ -14,9 +15,35 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="ScreenA" component={ScreenA} />
-        <Drawer.Screen name="ScreenB" component={ScreenB} />
+      <Drawer.Navigator
+        drawerStyle={{
+          backgroundColor: "#e6e6e6",
+          width: 250,
+        }}
+        edgeWidth={900}
+        initialRouteName="ScreenB"
+        hideStatueBar={false}
+      >
+        <Drawer.Screen
+          options={{
+            title: "This is Screen B Title",
+            drawerIcon: () => (
+              <Ionicons name="md-person" size={24} color="black" />
+            ),
+          }}
+          name="ScreenA"
+          component={ScreenA}
+        />
+        <Drawer.Screen
+          options={{
+            title: "This is Screen B Title",
+            drawerIcon: () => (
+              <Ionicons name="md-person" size={24} color="black" />
+            ),
+          }}
+          name="ScreenB"
+          component={ScreenB}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
