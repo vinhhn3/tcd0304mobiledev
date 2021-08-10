d2c29c355127516769b7b292efaa7bdd42515173
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-08-10 10:14:31 +0700

    Use Stack navigation

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
+import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "react-native-screens/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+function ScreenA({ navigation }) {
+  const onPressHanlder = () => {
+    navigation.navigate("ScreenB");
+  };
+
+  return (
+    <View>
+      <Text>ScreenA</Text>
+      <Button title="Go to Screen B" onPress={onPressHanlder} />
+    </View>
+  );
+}
+
+function ScreenB({ navigation }) {
+  const onPressHanlder = () => {
+    navigation.navigate("ScreenA");
+  };
+
+  return (
+    <View>
+      <Text>ScreenB</Text>
+      <Button title="Go to Screen A" onPress={onPressHanlder} />
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="ScreenA" component={ScreenA} />
+        <Stack.Screen name="ScreenB" component={ScreenB} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
