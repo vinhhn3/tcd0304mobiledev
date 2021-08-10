e393226a07436d7004e4c11ad65f917365060397
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-08-10 10:41:39 +0700

    Use TabNavigation and vector icons

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { StyleSheet } from "react-native";
@@ -6,13 +8,37 @@ import ScreenA from "./screens/ScreenA";
 import ScreenB from "./screens/ScreenB";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="ScreenA" component={ScreenA} />
-        <Stack.Screen name="ScreenB" component={ScreenB} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused }) => {
+            if (route.name === "ScreenA") {
+              return (
+                <FontAwesome
+                  name="bars"
+                  size={24}
+                  color={focused ? "#f0f" : "#555"}
+                />
+              );
+            } else if (route.name === "ScreenB") {
+              return (
+                <Ionicons
+                  name="md-person"
+                  size={24}
+                  color={focused ? "#f0f" : "#555"}
+                />
+              );
+            }
+          },
+        })}
+      >
+        <Tab.Screen name="ScreenA" component={ScreenA} />
+        <Tab.Screen name="ScreenB" component={ScreenB} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
